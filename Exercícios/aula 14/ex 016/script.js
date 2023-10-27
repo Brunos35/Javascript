fa4d9f01e1e86d4fad7790ebadfe663c4a78e266
Brunos35 <87710790+Brunos35@users.noreply.github.com>
2023-10-27 15:59:16 -0300

    ex 016 pronto

--- Exercícios/aula 14/ex 016/script.js
+++ Exercícios/aula 14/ex 016/script.js
@@ -1,1 +1,37 @@
-alert('olá')
+function contar(){
+    var inicio = window.document.getElementById('txtn1')
+    var fim = window.document.getElementById('txtn2')
+    var passo = window.document.getElementById('txtpasso')
+    var res = document.getElementById('res')
+    res.style.padding = '5px'
+
+    if(inicio.value.length == 0|| fim.value.length == 0 || passo.value.length == 0){
+    res.innerHTML = 'Impossivel contar'
+    //alert('[ERRO] Faltam dados')
+    
+    }else{
+        res.innerHTML = `Contando: <br>`
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+
+        if(passo<=0){
+            alert = ('Passo inválido! Coniderando PASSO igual a 1')
+            p = 1
+        }
+
+        if(i < f){
+
+            for(var c = i; c <= f ; c += p){//contagem crescente
+                res.innerHTML += ` ${c} \u{1F449}`  
+            }
+        }else{
+
+            for(var c = i; c >= f ; c -= p){//contagem decrescente
+                res.innerHTML += ` ${c} \u{1F449}`  
+            }
+            
+        }
+        res.innerHTML += `\u{1F44D}`
+    }
+}
