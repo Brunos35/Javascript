750e3b650d9d3d8d9aba22c2b1ee7619411078d5
Brunos35 <87710790+Brunos35@users.noreply.github.com>
2023-10-27 16:59:45 -0300

    ex017 pronto

--- Exercícios/aula 14/ex 017/script.js
+++ Exercícios/aula 14/ex 017/script.js
@@ -1,1 +1,20 @@
 alert('olá')
+function calcular(){
+var num = document.getElementById('txtn')
+tab = document.getElementById('seltab')
+
+if(num.value.length == 0){
+    alert('Insira um Número!')
+}else{
+    var n = Number(num.value)
+    tab.innerHTML = ''
+    for(var c = 1; c <= 10; c++){
+    var item = document.createElement('option')
+    item.text = `${n} x ${c} = ${n*c}`
+    item.value = `tab${c}`
+    tab.appendChild(item)
+    }
+}
+
+
+}
