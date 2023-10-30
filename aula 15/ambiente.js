c3d318727e400a155f9e8345cee840ed7b63f079
Brunos35 <87710790+Brunos35@users.noreply.github.com>
2023-10-30 16:51:41 -0300

    aula 15

---
+++ aula 15/ambiente.js
@@ -0,0 +1,12 @@
+let num = [5,6,3,2,1]
+/*
+num.push(8)
+console.log(`Nosso vetor é ${num} ${num.length} ${num.sort()}`)
+*/
+/*
+for (let pos = 0; pos <= num.length;pos++){
+    console.log(num[pos])
+}*/
+for(let pos in num){
+    console.log(num[pos])
+}
