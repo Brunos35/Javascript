41c4a3bf88ec4626d7728c0cd67e31f57723e343
Brunos35 <87710790+Brunos35@users.noreply.github.com>
2023-11-01 15:50:20 -0300

    aula 17 atualizado

--- Exercícios/aula 17/script.js
+++ Exercícios/aula 17/script.js
@@ -2,6 +2,7 @@ let num = document.getElementById('fnum')
 let list= document.getElementById('sellist')
 let res = document.querySelector('div#res')
 let valores = []
+let opt = document.getElementById('opt')
 
 function isNumero(n){
     if(Number(n)>= 1 && Number(n)<= 100){
@@ -12,7 +13,7 @@ function isNumero(n){
 }
 
 function inLista(n, l){
-    if(l.indexOf(Number(n))!= -1){
+    if(l.indexOf(Number(n)) != -1){
         return true
     }else{
         return false
@@ -22,14 +23,45 @@ function inLista(n, l){
 
 function adicionar(){
     
-    if(isNumero(num.value) && !inLista(num.value)){
-        window.alert('Tudo Ok')
+    if(isNumero(num.value) && !inLista(num.value , valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        list.appendChild(item)
+        res.innerHTML = ''
     }else{
     window.alert('Valor inválido ou já encontrado na lista')
     }
-
+    num.value = ''
+    num.focus()
 }
 
 function finalizar(){
+    
+    if(valores.length == 0){
+        window.alert('Adicione um valor antes de finalizar')
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior){
+            maior = valores[pos]
+            }
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        media = soma / tot
+        res.innerHTML =''
+        res.innerHTML += `<p>Ao todo temos ${tot} número cadastrados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores temor ${soma}</p>`
+
+    }
 
 }
